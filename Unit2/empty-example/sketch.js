dfd73ba543b51f8eaabb88db914c71a968e50524
dbroeksmit <dbroeksmit@hotmail.com>
2021-09-21 11:50:49 -0500

    hey

--- Unit2/empty-example/sketch.js
+++ Unit2/empty-example/sketch.js
@@ -1,16 +1,92 @@
-let x = 0 ;
+let state = 0;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background('grey')
-  rect(x, 100, 100, 100)
-  x=x+3 ;
-  if (x>width){
-  x=0;
-}
+  background(220);
+
+  switch (state) {
+    case 0:
+      background("black");
+      text("state 0", 100, 100);
+
+      for (var j = 0; j <= height; j += 20) {
+        for (var i = 0; i <= width; i += 20) {
+          rect(i, j, 5, 5);
+        }
+      }
+
+      break;
+
+    case 1:
+      background("yellow");
+      text("state 1", 100, 100);
+
+      for (var j = 0; j <= height; j += 20) {
+        for (var i = 0; i <= width; i += 20) {
+          ellipse(i, j, 10, 10);
+        }
+      }
+
+      break;
+
+    case 2:
+      background("blue");
+      text("state 2", 100, 100);
+
+      for (var j = 0; j <= height; j += 20) {
+        for (var i = 0; i <= width; i += 20) {
+          triangle(i, j, 10, 10);
+        }
+      }
+
+      break;
+
+    case 3:
+      background("purple");
+      text("state 3", 100, 100);
+
+      for (var j = 0; j <= height; j += 20) {
+        for (var i = 0; i <= width; i += 20) {
+          rect(i, j, 75, 5);
+        }
+      }
+
+      break;
+
+    case 4:
+      background("pink");
+      text("state 4", 100, 100);
+
+      for (var j = 0; j <= height; j += 20) {
+        for (var i = 0; i <= width; i += 20) {
+          rect(i, j,5, 75);
+        }
+      }
+
+      break;
+
+    case 5:
+      background("white");
+      text("state 5", 100, 100);
+
+      for (var j = 0; j <= height; j += 20) {
+        for (var i = 0; i <= width; i += 20) {
+          rect(i, j, 10, 10);
+        }
+      }
+      break;
+  }
 
 
+    }
+
+  function mouseReleased() {
+    state++;
+    if (state > 4) {
+      state = 0;
+
+  }
 }

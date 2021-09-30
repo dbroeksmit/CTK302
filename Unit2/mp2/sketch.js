55823fb3096cbe42ba400b8c3cde9e5f20d0edc4
dbroeksmit <dbroeksmit@hotmail.com>
2021-09-30 11:48:56 -0500

    mp2 boiii

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -5,7 +5,7 @@ function setup() {
   dog = loadImage("assets/dog.jpg") ;
   bridge = loadImage("assets/bridge.jpg") ;
   sky = loadImage("assets/sky.jpg") ;
-  dude = loadImage("assets/building.jpg") ;
+  building = loadImage("assets/building.jpg") ;
   imageMode(CENTER) ;
 
 }
@@ -13,29 +13,31 @@ function setup() {
 function draw() {
   clear();
   numberOfTouches = touches.length;
-  text(numberOfTouches + ' touches', 5, 10);
+//  text(numberOfTouches + ' touches', 5, 10);
+
 
   switch(numberOfTouches) {
     case 0:
       image(dog, width/2, height/2, 200, 200) ;
-      text("say hello to my puppy chico", 5, 22) ;
+      text("say hello to my puppy chico", 40, 62) ;
+      textSize(20);
       break ;
 
       case 1:
       // image()
-       text("i like to travel and take pictures of cool things like this bridge", 5, 22) ;
+       text("i like to travel and take pictures of cool things like this bridge", 40, 62) ;
        image(bridge, width/2, height/2, 200, 200) ;
       break ;
 
       case 2:
       // (image)
-      text("i also enjoy nature check out this pretty sunset", 5, 22) ;
+      text("i also enjoy nature check out this sunset", 40, 62) ;
       image(sky, width/2, height/2, 200, 200) ;
       break ;
 
       case 3:
       // image()
-     text("and lastly i enjoy architecture", 5, 22) ;
+     text("and lastly i enjoy architecture", 40, 62) ;
      image(building, width/2, height/2, 200, 200) ;
       break ;
 
